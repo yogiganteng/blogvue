39652db9c2522e4a1882316cedf71326e5641f39
yogimardilah <yogimardilah19@gmail.com>
2020-12-03 10:32:47 +0700

    laravel vue

---
+++ resources/views/latihan1.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laravel Vue</title>
+<style>
+body{
+    font-family : sans-serif;
+    margin : 15%;
+}
+</style>
+</head>
+<body>
+    
+    <div id="app"> 
+    <h1> Hello Laravel</h1>
+    <example-component></example-component>
+    </div>
+    <script src="{{  asset('js/app.js') }}"></script>
+</body>
+</html>
